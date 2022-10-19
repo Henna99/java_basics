66af7038598950c4aa2573a5f03d3a40a75bd9fa
henna <hennam99@outlook.com>
2022-10-19 14:52:22 +0100

    VAT Task

--- src/main/java/com/lbg/coh2/App.java
+++ src/main/java/com/lbg/coh2/App.java
@@ -1,7 +1,7 @@
 package com.lbg.coh2;
 
 import java.io.IOException;
-import java.io.BufferedReader;
+import java.util.Scanner;
 import java.io.InputStreamReader;
 
 /**
@@ -12,14 +12,22 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-       System.out.print( "Enter a message:  " );
-        
-       BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        
-       String dataRead= br.readLine();
+      
+       System.out.print( "Enter item cost:  " );
+       Scanner console= new Scanner (System.in); 
+       double cost=console.nextDouble();
+       System.out.println("Enter VAT rate:  ");
+       double vat=console.nextDouble();
+       App app= new App();
        
-       System.out.println(" =>" + dataRead);
-        
+       System.out.println("Total is " + app.VAT(cost,vat));
+       
+    }
+    
+    
+    double VAT (double cost, double vat)
+    {
+    	return cost*(1+(vat/100));
     }
      
    
